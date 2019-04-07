2555178f494605b3d1ea408d6b92c3ec99533833
Joe Boulos <jab12288@gmail.com>
2019-04-07 15:56:35 -0700

    created model for db, code cleanup

--- server/models/users.js
+++ server/models/users.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const onlineDb = require('../../config/keys');
 
 mongoose.connect(onlineDb.mongoURI, { useNewUrlParser: true });
+
 const db = mongoose.connection;
+
 mongoose.Promise = global.Promise;
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('MongoDB is connected to the Users database');
