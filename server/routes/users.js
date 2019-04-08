fca25cdf5a825b934f3731cbb3737d34adff5a3b
Joe Boulos <jab12288@gmail.com>
2019-04-07 20:20:58 -0700

    added post and put routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,18 +4,63 @@ const router = express.Router();
 // bring in item model
 const User = require('../models/User');
 
-// create routes
 
-// @route GET /pageOne
-
-// @route GET /checkout
+// @route GET /allusers
 router.get('/allusers', (req, res, next) => {
-  // take the model and use find method to get all users
   User.find()
-    .limit(10)
-    .then(users => res.json({test}))
+    .then(users => res.json(users))
     .catch(err => console.log('error: ', err))
 })
 
+
+// @route POST /pageOne
+router.post('/pageone', (req, res, next) => {
+const { name, email, password } = req.body
+
+  const pageOne = new User({name, email, password})
+  pageOne.save().then(user => res.json(user))
+});
+
+// @route PUT /pagetwo
+router.put('/pagetwo', (req, res, next) => {
+  const { email, addresslineone, addresslinetwo, city, state, zip, phone } = req.body
+
+    User.updateOne({email:"email0"},
+      {
+        $set:
+          {
+            email,
+            addresslineone,
+            addresslinetwo,
+            city,
+            state,
+            zip,
+            phone
+          }
+      })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(err =>  console.log(err))
+  });
+
+// @ route PUT /pagethree
+router.put('/pagethree', (req, res, next) => {
+  const { email, creditcard, expirationdate, cvv, billingzipcode} = req.body
+    User.updateOne({email:"email0"},
+      {
+        $set:
+          {
+            creditcard,
+            expirationdate,
+            cvv,
+            billingzipcode
+          }
+      })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(err => console.log(err))
+  });
 
 module.exports = router;
