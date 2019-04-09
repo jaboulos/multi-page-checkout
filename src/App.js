3899d2fdde0151793e53c3e7eef534a304faa753
Joe Boulos <jab12288@gmail.com>
2019-04-08 21:06:35 -0700

    removed commented code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
-// import React, { Component } from 'react';
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>Test</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
 import React from 'react';
 
 const App = (props) => {
