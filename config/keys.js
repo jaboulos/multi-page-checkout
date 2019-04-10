06766167eed168113131ea0710104c3f1138c614
Joe Boulos <jab12288@gmail.com>
2019-04-09 18:30:31 -0700

    upd

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: `mongodb+srv://joe:joe123@checkout-1akai.mongodb.net/test?retryWrites=true`
+  mongoURI: `mongodb+srv://joe:joe123@checkoutapp-1akai.mongodb.net/test?retryWrites=true`
 }
