c24e8da6ea88210cb311d86e0715364f49849346
Joe Boulos <jab12288@gmail.com>
2019-04-11 17:21:07 -0700

    added get and post route for order collection

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // bring in item model
 const User = require('../models/User');
-
+const Order = require('../models/Order');
 
 // @route GET /allusers
 router.get('/allusers', (req, res, next) => {
@@ -12,12 +12,28 @@ router.get('/allusers', (req, res, next) => {
     .catch(err => console.log('error: ', err))
 })
 
+// @route GET /userorders
+router.get('/userorders', (req, res, next) => {
+  const { email } = req.body
+  //User.find({email}, {$exists:true})
+  Order.find({email })
+    .sort({date: -1})
+    .then(orders=> res.json(orders))
+    .catch(err => console.log('error: ', err))
+})
 
 // @route POST /submit
 router.post('/saveinfo', (req, res, next) => {
   const saveInfo = new User(req.body)
   saveInfo.save().then(user => res.json(user))
 });
+
+// @route POST /order
+router.post('/order', (req, res, next) => {
+  const orderInfo = new Order(req.body)
+  orderInfo.save().then(order => res.json(order))
+});
+
 
 // @route PUT /pagetwo
 router.put('/pagetwo', (req, res, next) => {
@@ -40,23 +56,5 @@ router.put('/pagetwo', (req, res, next) => {
     .catch(err =>  console.log(err))
   });
 
-// @ route PUT /pagethree
-router.put('/pagethree', (req, res, next) => {
-  const { email, creditcard, expirationdate, cvv, billingzipcode} = req.body
-    User.updateOne({email:email},
-      {
-        $set:
-          {
-            creditcard,
-            expirationdate,
-            cvv,
-            billingzipcode
-          }
-      })
-    .then((data) => {
-      res.json(data);
-    })
-    .catch(err => console.log(err))
-  });
 
 module.exports = router;
