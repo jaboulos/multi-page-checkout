2cc2134f0649011587f8e01cf009d81ee913b18c
Joe Boulos <jab12288@gmail.com>
2019-04-14 18:24:34 -0700

    updated, includes browserrouter for navbar

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter, Route } from 'react-router-dom';
-
-// import App from './App';
-// import Homepage from './components/Homepage';
-// // import StepOne from './components/StepOne';
-
-// import * as serviceWorker from './serviceWorker';
-
-
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App>
-//       <Route exact path="/" component={Homepage}/>
-//       <Route exact path='/homepage' component={Homepage}/>
-//     </App>
-//   </BrowserRouter>,
-//   document.getElementById('root'));
-
-// serviceWorker.unregister();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './App';
-
-// import StepOne from './components/StepOne';
+import Homepage from './components/Homepage';
 
 import * as serviceWorker from './serviceWorker';
 
-
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App>
+      <Route exact path="/" component={Homepage}/>
+      <Route exact path='/homepage' component={Homepage}/>
+    </App>
+  </BrowserRouter>,
+  document.getElementById('root'));
 
 serviceWorker.unregister();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter, Route } from 'react-router-dom';
+
+// import App from './App';
+
+
+// import * as serviceWorker from './serviceWorker';
+
+
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
+
+// serviceWorker.unregister();
 
