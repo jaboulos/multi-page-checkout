955a09c20247d37ea848409dd3b0cab94fa75f5c
Joe Boulos <jab12288@gmail.com>
2019-04-15 17:04:40 -0700

    updates to input fields and props passed from Homepage component

--- src/components/CheckInfo.js
+++ src/components/CheckInfo.js
@@ -1,22 +1,24 @@
 import React from 'react';
 
-const CheckInfo = ({name, email, password, currentPage, addresslineone, addresslinetwo, city, state, zip, phone, creditcard, expirationdate, cvv, billingzipcode, handleHomePage, handleSubmit, goBack }) =>{
-  console.log("check info: ", name, {email})
+const CheckInfo = ({firstName, lastName, company, price, currentPage, addresslineone, addresslinetwo, city, state, zip, phone, creditcard, creditcardnumber, expirationdate, cvv, billingzipcode, handleHomePage, handleSubmit, goBack }) =>{
+  // console.log("check info: ", name, {email})
     return (
       <div>
         <div>
           <h4>Verify The Following is correct</h4>
           <p>Current Step: {currentPage}</p>
-          <p>Name: {name}</p>
-          <p>Email: {email}</p>
-          <p>Password: {password}</p>
+          <p>First Name: {firstName}</p>
+          <p>Last Name: {lastName}</p>
+          <p>Company: {company}</p>
+          <p>Purchase Cost: {price}</p>
           <p>Address Line 1: {addresslineone}</p>
           <p>Address Line 2: {addresslinetwo}</p>
           <p>City: {city}</p>
           <p>State: {state}</p>
           <p>Zip: {zip}</p>
           <p>Phone Number: {phone}</p>
-          <p>Credit Card Number: {creditcard}</p>
+          <p>Credit Card: {creditcard}</p>
+          <p>Credit Card number: {creditcardnumber}</p>
           <p>CC Expiration Date: {expirationdate}</p>
           <p>CVV: {cvv}</p>
           <p>Billing Zip Code: {billingzipcode}</p>
